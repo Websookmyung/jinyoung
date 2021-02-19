3d6e7b827ff1c777858eefed888e95bea2e76d97
KIM JINYOUNG <khappy9888@sookmyung.ac.kr>
2021-02-19 13:55:37 +0900

    예제 14-5 14-6 아이디 중복 검사 함수와 회원정보 저장

--- signup.js
+++ signup.js
@@ -26,3 +26,23 @@ function duplicationCheck(){
         alert("You can use " + username_in + "as a username.");
     }
 }
+
+document.getElementById("signup_button").onclick=signup;
+
+function signup(){
+    username_in=document.getElementById("username2").value;
+    password_in1=document.getElementById("pass1").value;
+    var localStorage=window.localStorage;
+    if (!localStorage){
+        //local storage is not supported by this browser.
+        //do nothing
+    }
+    else{
+        numUsers = localStorage.numUsers;
+        if (numUsers == undefined) numUsers ="0";
+        localStorage["user"+numUsers]=username_in;
+        localStorage["pass"+numUsers]=password_in1;
+        localStorage.numUsers=parseInt(numUsers)+1;
+        alert("Signup succeeded!\n"+"Number of users: "+ localStorage.numUsers);
+    }
+}
